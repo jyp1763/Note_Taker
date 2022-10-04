1b77fa3f91889bd4f3dae29a48714f0b87065dae
jyp1763 <108202344+jyp1763@users.noreply.github.com>
2022-10-03 23:10:41 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -14,6 +14,39 @@ app.use(express.json());
 app.use('/', htmlRoutes);
 app.use('/', notes);
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      if (err) throw err;
+      res.json(JSON.parse(data));
+    });
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname,  "./db/db.json"), 'utf8', (err, data) => {
+      let db = JSON.parse(data);
+      db.push({
+        id: uuid.v4(),
+        ...req.body,
+      });
+      fs.writeFile(
+        path.join(__dirname,  "./db/db.json"),
+        JSON.stringify(db, null, 2),
+        (err, data) => {
+          if (err) throw err;
+          res.json(db);
+        }
+      );
+    });
+  });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}. Welcome!`);
   });
